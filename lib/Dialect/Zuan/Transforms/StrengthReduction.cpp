1683b2a1978be36874ae0ce50488c0b91a2c644a
Juni May <juni_may@outlook.com>
2025-03-27 11:43:12 +0800

    [Zuan] Strength reduction for gather operation

--- lib/Dialect/Zuan/Transforms/StrengthReduction.cpp
+++ lib/Dialect/Zuan/Transforms/StrengthReduction.cpp
@@ -2,11 +2,13 @@
 #include "Zuan/IR/Zuan.h"
 #include "Zuan/Utils/ConvertToVP.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/OperationSupport.h"
 #include "mlir/IR/PatternMatch.h"
+#include "mlir/IR/ValueRange.h"
 #include "mlir/Support/LLVM.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "llvm/ADT/SmallVector.h"
@@ -154,12 +156,131 @@ struct SplatElementwisePattern : OpTraitRewritePattern<OpTrait::Elementwise> {
   }
 };
 
+/// Canonicalize addition of step and splat to a single step with modified
+/// start.
+struct SplatStepAddPattern : OpRewritePattern<arith::AddIOp> {
+  using OpRewritePattern<arith::AddIOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(arith::AddIOp op,
+                                PatternRewriter &rewriter) const override {
+    // step + splat -> step with start as splat + old start
+    auto lhs = op.getOperand(0);
+    auto rhs = op.getOperand(1);
+
+    SplatOp splat;
+    StepOp step;
+
+    if (auto splatOp = lhs.getDefiningOp<SplatOp>()) {
+      splat = splatOp;
+    } else if (auto splatOp = rhs.getDefiningOp<SplatOp>()) {
+      splat = splatOp;
+    } else {
+      return rewriter.notifyMatchFailure(op, "non-splat operand");
+    }
+
+    if (auto stepOp = lhs.getDefiningOp<StepOp>()) {
+      step = stepOp;
+    } else if (auto stepOp = rhs.getDefiningOp<StepOp>()) {
+      step = stepOp;
+    } else {
+      return rewriter.notifyMatchFailure(op, "non-step operand");
+    }
+
+    auto splatted = splat.getValue();
+    auto start = step.getStart();
+
+    auto newStart =
+        rewriter.create<arith::AddIOp>(op.getLoc(), splatted, start);
+    auto newStep = rewriter.create<StepOp>(op.getLoc(), newStart,
+                                           step.getDim().getZExtValue(),
+                                           step.getMixedSizes());
+    rewriter.replaceOp(op, newStep);
+    return success();
+  }
+};
+
+/// Convert gather to load when all indices are splats or steps along the
+/// corresponding dimension.
+struct GatherToLoadPattern : OpRewritePattern<GatherOp> {
+  using OpRewritePattern<GatherOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(GatherOp op,
+                                PatternRewriter &rewriter) const override {
+    // Each index:
+    // - splat -> subview with splatted value as offset, 0 as stride
+    // - step or scaled step -> contiguous or strided
+    SmallVector<OpFoldResult> offsets;
+    SmallVector<OpFoldResult> sizes;
+    SmallVector<OpFoldResult> strides;
+
+    // Leading indices are all splats, the last index is a step.
+    auto indices = op.getIndices();
+    auto memref = op.getBase();
+
+    if (indices.empty()) {
+      // load the scalar and splat with empty sizes.
+      auto load =
+          rewriter.create<memref::LoadOp>(op.getLoc(), memref, ValueRange{});
+      auto splat =
+          rewriter.create<SplatOp>(op.getLoc(), load, ArrayRef<int64_t>());
+      rewriter.replaceOp(op, splat);
+      return success();
+    }
+
+    // Check leading indices.
+    for (unsigned i = 0, e = indices.size(); i < e; ++i) {
+      auto index = indices[i];
+      if (auto splatOp = index.getDefiningOp<SplatOp>()) {
+        auto splatSizes = splatOp.getMixedDims();
+
+        if (splatSizes.size() != indices.size()) {
+          return rewriter.notifyMatchFailure(
+              op, "splat rank and memref rank mismatch");
+        }
+
+        offsets.push_back(splatOp.getValue());
+        sizes.push_back(splatSizes[i]);
+        strides.push_back(rewriter.getIndexAttr(0));
+      } else if (auto stepOp = index.getDefiningOp<StepOp>()) {
+        auto stepSizes = stepOp.getMixedSizes();
+        auto dim = stepOp.getDim();
+        auto start = stepOp.getStart();
+
+        if (stepSizes.size() != indices.size()) {
+          return rewriter.notifyMatchFailure(
+              op, "step rank and memref rank mismatch");
+        }
+
+        if (dim.getZExtValue() != i) {
+          return rewriter.notifyMatchFailure(op,
+                                             "not step along the dimension");
+        }
+
+        offsets.push_back(start);
+        sizes.push_back(stepSizes[i]);
+        strides.push_back(rewriter.getIndexAttr(1));
+      } else {
+        // TODO: support scaled step as stride.
+        return rewriter.notifyMatchFailure(op, "non-splat index");
+      }
+    }
+    // Create subview.
+    auto subview = rewriter.create<memref::SubViewOp>(op.getLoc(), memref,
+                                                      offsets, sizes, strides);
+    auto load = rewriter.create<LoadOp>(op.getLoc(), subview);
+
+    rewriter.replaceOp(op, load);
+    return success();
+  }
+};
+
 } // namespace
 
 void populateZuanStrengthReductionPatterns(RewritePatternSet &patterns) {
-  patterns.add<StepToSplatPattern>(patterns.getContext());
-  patterns.add<SplatCastPattern, StepCastPattern>(patterns.getContext());
-  patterns.add<SplatElementwisePattern>(patterns.getContext());
+  patterns
+      .add<StepToSplatPattern, SplatCastPattern, StepCastPattern,
+           SplatElementwisePattern, SplatStepAddPattern, GatherToLoadPattern>(
+          patterns.getContext());
 }
 
 void ZuanStrengthReductionPass::runOnOperation() {
